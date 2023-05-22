7b80d6077a402014ddc2ff86b67b980ac8c631fe
linkit@1196 <trinhngoclinh0308@gmail.com>
2023-05-22 23:18:12 +0700

    done day 1

---
+++ src/main/java/_1_ContainDuplicate.java
@@ -0,0 +1,38 @@
+import java.util.HashMap;
+
+public class _1_ContainDuplicate {
+
+    public static void main(String[] args) {
+        long startTime = System.nanoTime();
+        // start here
+        int[] array = {1, 2, 3, 4, 5};
+        System.out.println(runSolution(array));
+        long endTime = System.nanoTime();
+        long totalTime = endTime - startTime;
+        System.out.println("run time: " + totalTime);
+    }
+
+    public static boolean runSolution_1(int[] nums) {
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[i] == nums[j]) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    // faster
+    public static boolean runSolution(int[] nums) {
+        HashMap<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums.length; i++) {
+            if(map.containsKey(nums[i])) {
+                return true;
+            }
+            map.put(nums[i], 1);
+        }
+        return false;
+    }
+
+}
---
+++ src/main/java/_2_ValidAnagram.java
@@ -0,0 +1,60 @@
+import java.util.HashMap;
+
+public class _2_ValidAnagram {
+
+    public static void main(String[] args) {
+        long startTime = System.currentTimeMillis();
+
+        // start here
+        String s = "anagramww";
+        String t = "wnagaramq";
+        System.out.println(Solution_1.isAnagram(s, t));
+
+        long endTime = System.currentTimeMillis();
+        long totalTime = endTime - startTime;
+        System.out.println("run time: " + totalTime);
+    }
+
+    public static class Solution {
+        public static boolean isAnagram(String s, String t) {
+            String[] sArr = s.split("");
+            HashMap<String, Integer> sMap = countDistinctChar(sArr);
+            String[] tArr = t.split("");
+            HashMap<String, Integer> tMap = countDistinctChar(tArr);
+            return sMap.entrySet().equals(tMap.entrySet());
+        }
+
+        private static HashMap<String, Integer> countDistinctChar(String[] s) {
+            HashMap<String, Integer> output = new HashMap<>();
+            for (int i = 0; i < s.length; i++) {
+                String key = s[i];
+                if (output.containsKey(key)) {
+                    output.put(key, output.get(key) + 1);
+                } else {
+                    output.put(key, 1);
+                }
+            }
+            return output;
+        }
+    }
+
+    //internet solution
+    //To solve the anagram problem, we can count the frequency of each letter in both strings and compare them.
+    //If the frequencies of all letters are the same in both strings, then the two strings are anagrams of each other.
+    public static class Solution_1 {
+        public static boolean isAnagram(String s, String t) {
+            if (s.length() != t.length()) {
+                return false;
+            }
+            int[] alphabet = new int[26];
+            for (int i = 0; i < s.length(); i++)
+            {
+                alphabet[s.charAt(i) - 'a']++;
+                alphabet[t.charAt(i) - 'a']--;
+            }
+            for (int i : alphabet) if (i != 0) return false;
+            return true;
+        }
+    }
+
+}
