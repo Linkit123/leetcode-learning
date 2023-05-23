41c4008237ed8cddbe1117e8cba264fac625d00c
linkit@1196 <trinhngoclinh0308@gmail.com>
2023-05-24 00:05:27 +0700

    two sum

---
+++ src/main/java/_3_TowSum.java
@@ -0,0 +1,26 @@
+import java.util.Arrays;
+import java.util.HashMap;
+
+public class _3_TowSum {
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 5, 2, 3, 3};
+        int target = 6;
+        System.out.println(Arrays.toString(twoSum(arr, target)));
+    }
+
+    //Time complexity: O(N^2);
+    //Space Complexity: O(1);
+    public static int[] twoSum(int[] nums, int target) {
+        int[] arr = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = 0; j < i; j++) {
+                if ((nums[i] + nums[j]) == target) {
+                    arr[0] = i;
+                    arr[1] = j;
+                    return arr;
+                }
+            }
+        }
+        return arr;
+    }
+}
