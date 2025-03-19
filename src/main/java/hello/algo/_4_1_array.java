dcde32792b0bc32150d8ca19aa8cf7871aede029
linhtn <linh.tn@mbageas.life>
2025-03-19 09:04:24 +0700

    [hello-algo] fix 4.1 insert array

--- src/main/java/hello/algo/_4_1_array.java
+++ src/main/java/hello/algo/_4_1_array.java
@@ -1,19 +1,19 @@
 package hello.algo;
 
-import java.util.Stack;
+import utils.Utils;
 
 public class _4_1_array {
 
     public static void main(String[] args) {
-        int[] num = new int[] {1,2,5,4,0};
-        insert(3, 2, num);
+        int[] num = new int[] {1, 2, 5, 4, 0};
+        insert(3, 0, num);
     }
 
     static void insert(int element, int index, int[] num) {
-        for(int i = num.length - 1; i >= index ; i--) {
+        for(int i = num.length - 1; i > index ; i--) {
             num[i] = num[i - 1];
         }
         num[index] = element;
-        System.out.println(num);
+        Utils.printArray(num);
     }
 }
