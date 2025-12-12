03d84b7a12209c46375a6526d219e93a00c5a504
linh.trinh <linh.trinh@itech.asia>
2025-12-12 17:39:49 +0700

    update

--- src/main/java/test/assignment/Inspius/RestfulExample.java
+++ src/main/java/test/assignment/Inspius/RestfulExample.java
@@ -3,54 +3,48 @@ package test.assignment.Inspius;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.*;
 
 @SpringBootApplication
 public class RestfulExample {
-
   public static void main(String[] args) {
     SpringApplication.run(RestfulExample.class, args);
   }
-}
 
-@RestController
-@RequestMapping("/api/v1")
-class FilterController {
+  List<String> items = Arrays.asList("apple", "banana", "apricot", "blueberry", "orange");
 
-  private final List<String> items = Arrays.asList("apple", "banana", "apricot", "blueberry", "orange");
+  @RestController
+  @RequestMapping("/api/v1")
+  public class ExController {
 
-  @PostMapping("/filterItems")
-  public List<String> filterItems(@RequestBody FilterRequest request) {
-    List<String> result = new ArrayList<>();
-    String filterCondition = request.getFilterCondition();
+    @PostMapping("/filterItems")
+    public List<String> filterItems(@RequestBody FilterRequest request) {
 
-    for (String item : items) {
-      if (item.contains(filterCondition)) {
-        result.add(item);
+      var response = new ArrayList<String>();
+      for (String item : items) {
+        if (item.contains(request.getFilterCondition())) {
+          response.add(item);
+        }
       }
+
+      return response;
     }
-    return result;
   }
 }
 
 class FilterRequest {
-
   private String filterCondition;
 
-  public FilterRequest() {
-  }
-
-  public FilterRequest(String filterCondition) {
-    this.filterCondition = filterCondition;
-  }
-
+  // Getters and Setters
   public String getFilterCondition() {
     return filterCondition;
   }
 
-  public void setFilterCondition(String filterCondition) {
+  public void setFilterConditions(String filterCondition) {
     this.filterCondition = filterCondition;
   }
+
 }
